73de358a2fe620550feb1b1ef6647254f770f2e6
Nadiia <npodufala15@gmail.com>
2020-11-10 22:59:58 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './scss';
+
+import App from './App/app.scss';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
